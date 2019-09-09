c550655b6bb879c08049a12223ba202a0536fcce
Jasmine Williams <jasminewilliams2017@gmail.com>
2019-09-08 19:07:00 -0500

    added function with logic to make form appear when jumbotron button is clicked

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
 console.log('You are a wizard!');
+
+
+const makeFormAppear = (e) => {
+    if (e.target.id === 'jumbo-button') {
+        document.getElementById('form').style.display = "block";
+    }
+}
+
+document.getElementById('jumbo-button').addEventListener('click', makeFormAppear)
