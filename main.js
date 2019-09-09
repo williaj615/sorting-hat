450d6508b2c13ec18ea7a49235a1ff767f65c9c1
Jasmine Williams <jasminewilliams2017@gmail.com>
2019-09-08 19:49:41 -0500

    created function with logic that works to randomly sort students inputted into a house and print a card with their house and expel button

--- main.js
+++ main.js
@@ -1,5 +1,11 @@
 console.log('You are a wizard!');
 
+const hogHouses = ['Gryffindor', 'Ravenclaw', 'Slytherin', 'Hufflepuff']
+
+const printToDom = (divId, stringToPrint) => {
+    document.getElementById(divId).innerHTML = stringToPrint;
+}
+
 
 const makeFormAppear = (e) => {
     if (e.target.id === 'jumbo-button') {
@@ -8,3 +14,28 @@ const makeFormAppear = (e) => {
 }
 
 document.getElementById('jumbo-button').addEventListener('click', makeFormAppear)
+
+const sortStudentAndClear = (e) => {
+    if (e.target.id === 'sort') {
+    let studentHouse = hogHouses[Math.floor(Math.random() * hogHouses.length)];
+    let domString = '<div class="row">';
+    let studentName = document.getElementById('name-input').innerText
+    domString += `
+        <div class="col-3">
+            <div class="card">
+                <div class="card-body">
+                    <h5 class="card-title">${studentName}</h5>
+                    <p class="card-house">${studentHouse}</p>
+                    <button type="button" class="btn btn-danger">Expel</button>
+                </div>
+            </div>
+        </div>
+    </div>`;
+
+    printToDom('students-container', domString)
+    }
+
+    document.getElementById('form').reset();
+}
+
+document.getElementById('sort').addEventListener('click', sortStudentAndClear)
