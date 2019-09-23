5ba613a160487ca4b1ec0d29f4668810edb6b6cc
Jasmine Williams <jasminewilliams2017@gmail.com>
2019-09-23 18:28:43 -0500

    adde js logic for expel button, some remaining bugs

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 console.log('You are a wizard!');
 
+const init = () => {
+
 const hogHouses = ['Gryffindor', 'Ravenclaw', 'Slytherin', 'Hufflepuff']
 
 const printToDom = (divId, stringToPrint) => {
@@ -17,33 +19,37 @@ document.getElementById('jumbo-button').addEventListener('click', makeFormAppear
 
 let domString = '';
 
+
 const sortStudentAndClear = (e) => {
     if (e.target.id === 'sort') {
     let studentHouse = hogHouses[Math.floor(Math.random() * hogHouses.length)];
     let studentName = document.getElementById('name-input').value
     domString += `
             <div id="${studentName}-card" class="card">
-                <div class="card-body">
                     <h5 class="card-title">${studentName}</h5>
                     <p class="card-house">${studentHouse}</p>
-                    <button type="button" id="expel" class="btn btn-danger">Expel</button>
-                </div>
+                    <button type="button" class="btn btn-danger expel">Expel</button>
             </div>`
-
     printToDom('students-container', domString)
-    }
 
     document.getElementById('form').reset();
+
+    expelButtons = document.getElementsByClassName('expel');
+    for (var i = 0; i < expelButtons.length; i++) {
+        expelButtons[i].addEventListener('click', expelStudent);}
+}
 }
 
 document.getElementById('sort').addEventListener('click', sortStudentAndClear)
 
 
+
 const expelStudent = (e) => {
-    if (e.target.id === 'expel') {
-        let studentToExpel = e.target.parentNode
-        studentToExpel.style.display = 'none';
-        console.log('hi')
-    }
+  studentToExpel = e.target.parentElement.id
+document.getElementById(studentToExpel).style.display = 'none';
 }
-document.getElementById('expel').addEventListener('click', expelStudent)
+}
+//document.getElementById('expel').addEventListener('click', expelStudent);
+//document.getElementById(`expel-${studentName}`).addEventListener('click', expelStudent)
+
+init();
